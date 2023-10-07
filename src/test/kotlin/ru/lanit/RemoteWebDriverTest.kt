a5c0ec2ba8d231e10ec944dbfebf84f4b5b59131
Michael Mironov <nemiron94@gmail.com>
2023-10-07 19:36:44 +0300

    Add selenoid test

--- src/test/kotlin/ru/lanit/RemoteWebDriverTest.kt
+++ src/test/kotlin/ru/lanit/RemoteWebDriverTest.kt
@@ -3,6 +3,9 @@ package ru.lanit
 import io.github.bonigarcia.wdm.WebDriverManager.isOnline
 import org.assertj.core.api.Assumptions.assumeThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
@@ -10,11 +13,24 @@ import java.net.URL
 class RemoteWebDriverTest : BaseTest() {
 
     @BeforeEach
-    fun setup() {
-        val seleniumServerUrl = URL("http://localhost:4444/")
+    fun setup(testInfo: TestInfo) {
+        val seleniumServerUrl = URL("http://localhost:4444/wd/hub")
         assumeThat(isOnline(seleniumServerUrl)).isTrue
 
         val options = ChromeOptions()
+        val selenoidOptions = HashMap<String, Any>()
+
+        selenoidOptions["enableVNC"] = true
+        selenoidOptions["name"] = testInfo.displayName
+        options.setCapability("selenoid:options", selenoidOptions)
+
         driver = RemoteWebDriver(seleniumServerUrl, options)
+    }
+
+    @Test
+    @DisplayName("[WEB] Selenoid test")
+    fun testGrid() {
+        driver.get(BASE_URI)
+        println()
     }
 }
